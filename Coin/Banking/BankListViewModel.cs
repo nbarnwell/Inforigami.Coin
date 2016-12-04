2216b1d4cf98e216851dfd9d70791d4164bf08a7
Neil Barnwell <mailbox@neilbarnwell.co.uk>
2016-12-04 14:39:25 +0000

    Impl screen header control.

--- Coin/Banking/BankListViewModel.cs
+++ Coin/Banking/BankListViewModel.cs
@@ -4,10 +4,17 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Caliburn.Micro;
+using Coin.Shared;
 
 namespace Coin.Banking
 {
     public class BankListViewModel : Screen
     {
+        public ScreenHeaderViewModel Header { get; set; }
+
+        public BankListViewModel()
+        {
+            Header = new ScreenHeaderViewModel {HeaderText = "Banks"};
+        }
     }
 }
---
+++ Coin/Shared/ScreenHeaderViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Caliburn.Micro;
+
+namespace Coin.Shared
+{
+    public class ScreenHeaderViewModel : PropertyChangedBase
+    {
+        public string HeaderText { get; set; }
+    }
+}
