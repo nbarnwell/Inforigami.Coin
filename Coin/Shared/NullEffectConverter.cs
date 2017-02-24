082654006707438df21c0a4d3855ee0222274225
Neil Barnwell <mailbox@neilbarnwell.co.uk>
2017-02-24 03:06:46 +0000

    Background blur when dialog is open

---
+++ Coin/Shared/NullEffectConverter.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+using System.Windows.Media.Effects;
+
+namespace Coin.Shared
+{
+    public class NullEffectConverter : IValueConverter
+    {
+        public Effect EffectIfNull { get; set; }
+        public Effect EffectIfNotNull { get; set; }
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value == null)
+            {
+                return EffectIfNull;
+            }
+            else
+            {
+                return EffectIfNotNull;
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return null;
+        }
+    }
+}
